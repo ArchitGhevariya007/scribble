68eb830b453e939a4b8e6b8bca8f59c09b9a7792
Archit Ghevariya <ghevariyaarchit@gmail.com>
2023-04-28 23:39:21 +0530

    header changed

--- src/components/ContentTab.js
+++ src/components/ContentTab.js
@@ -1,11 +1,12 @@
 import React from "react";
-import { Box, Tab, Typography, Tabs } from "@mui/material";
-import TabPanel from '@mui/lab/TabPanel';
+import { Box, Tab, Typography,FormControl,InputLabel,Input,InputAdornment } from "@mui/material";
+import { TabPanel, TabContext, TabList } from "@mui/lab";
+
 import { useState } from "react";
 
 // Main function
 export default function ContentTab() {
-  const [value, setValue] = useState(1);
+  const [value, setValue] = useState("1");
 
   const handleChange = (event, newValue) => {
     setValue(newValue);
@@ -13,42 +14,47 @@ export default function ContentTab() {
 
   const style = {
     mainContainer: {
-      float:"left",
-      padding:3
+      padding: 3,
     },
-    tabs:{
-      color:"rgb(232, 232, 232)"
-    }
+    tabs: {
+      color: "rgb(232, 232, 232)",
+    },
+    HeaderContainer: {
+      padding:3,
+      maxWidth: "calc(100%-200px)",
+      display:"inline-",
+      color: "rgb(255, 255, 255)",
+    },
   };
 
   return (
-    <Box sx={style.mainContainer}>
+    <>
+      <Box sx={style.HeaderContainer}>
+        <Typography variant="h3">Hello</Typography>
 
-      <Box >
-        <Typography variant="h2">Hello</Typography>
+        <FormControl sx={{ m: 1 }} variant="standard">
+          <InputLabel htmlFor="standard-adornment-amount">Amount</InputLabel>
+          <Input
+            id="standard-adornment-amount"
+            startAdornment={<InputAdornment position="start">$</InputAdornment>}
+          />
+        </FormControl>
       </Box>
 
-      <Box sx={{ borderBottom: 1, borderColor: "#22262b",mt:3 }}>
-        <Tabs
-          value={value}
-          onChange={handleChange}
-        >
-          <Tab label="Todos" value="1" sx={style.tabs}/>
-          <Tab label="Notes" value="2" sx={style.tabs}/>
-          <Tab label="Link" value="3" sx={style.tabs}/>
-        </Tabs>
+      <Box sx={style.mainContainer}>
+        <TabContext value={value}>
+          <Box sx={{ borderBottom: 1, borderColor: "divider" }}>
+            <TabList onChange={handleChange}>
+              <Tab label="Todos" value="1" />
+              <Tab label="Item Two" value="2" />
+              <Tab label="Item Three" value="3" />
+            </TabList>
+          </Box>
+          <TabPanel value="1">Item One</TabPanel>
+          <TabPanel value="2">Item Two</TabPanel>
+          <TabPanel value="3">Item Three</TabPanel>
+        </TabContext>
       </Box>
-      
-        <TabPanel value="1" index={0}>
-          Item One
-        </TabPanel>
-        <TabPanel value="2" index={1}>
-          Item Two
-        </TabPanel>
-        <TabPanel value="3" index={2}>
-          Item Three
-        </TabPanel>
-
-    </Box>
+    </>
   );
 }
