66bf0e55c847fe137aac96b3afc3b810d8741e71
Archit Ghevariya <ghevariyaarchit@gmail.com>
2023-05-01 22:57:02 +0530

    task added to local storage

--- src/components/Todos.js
+++ src/components/Todos.js
@@ -1,57 +1,76 @@
-import React from "react";
+import React, { useState } from "react";
 import { Box, TextField, Stack, Button } from "@mui/material";
-
-
-
-
+import TodosList from "./TodosList";
 
 export default function Todos() {
-    const style = {
-        addBox: {
-            //   backgroundColor: "#2d3036",
-            borderRadius: 0.3,
-            padding: 1,
-        },
-        addTextField: {
-            backgroundColor: "transparent",
-            border: "2px #2d3036  solid",
-            borderRadius: 1,
-            input: {
-                color: "rgb(232, 232, 232)"
-            },
-            "& fieldset": { border: 'none' },
+  const style = {
+    addBox: {
+      //   backgroundColor: "#2d3036",
+      borderRadius: 0.3,
+      padding: 1,
+    },
+    addTextField: {
+      backgroundColor: "transparent",
+      border: "2px #2d3036  solid",
+      borderRadius: 1,
+      input: {
+        color: "rgb(232, 232, 232)",
+      },
+      "& fieldset": { border: "none" },
+    },
+    todolist: {
+      backgroundColor: "#2d3036",
+      padding: 1,
+      mt: 2,
+      borderRadius: 2,
+      minHeight: "48vh",
+      height: "auto",
+      marginBottom: "10px",
+    },
+  };
 
-        },
-        todotask: {
-            backgroundColor: "#2d3036",
-            padding: 1,
-            mt:2,
-            borderRadius:2
-        }
-    };
-    return (
-        <>
-            {/* Add Todo Text Box */}
-            <Box sx={style.addBox}>
-                <TextField
-                    placeholder="Enter Task"
-                    fullWidth
-                    sx={style.addTextField}
-                    InputProps={{
-                        endAdornment: (
-                            <Stack direction="row" spacing={1}>
-                                <Button variant="contained" disableRipple>
-                                    Add
-                                </Button>
-                            </Stack>
-                        ),
-                    }}
-                />
-            </Box>
 
-            <Box sx={style.todotask }>
-            
-            </Box>
-        </>
-    );
+//   Add todo text
+  const [TodoText, SetTodoText] = useState("");
+  const [AddTodoText,SetAddTodoText]=useState(TodoText);
+
+
+  const AddTodo = (e) => {
+    SetAddTodoText(TodoText);
+    console.log(AddTodoText);
+    localStorage.setItem('Task', TodoText);
+    SetTodoText("");
+    e.preventDefault();
+  };
+
+  return (
+    <>
+
+      {/* Add Todo Text Box */}
+      <Box sx={style.addBox}>
+        <TextField
+          placeholder="Enter Task"
+          sx={style.addTextField}
+          fullWidth
+          value={TodoText}
+          onChange={(e) => {
+            SetTodoText(e.target.value);
+          }}
+          InputProps={{
+            endAdornment: (
+              <Stack direction="row" spacing={1}>
+                <Button variant="contained" disableRipple onClick={AddTodo}>
+                  Add
+                </Button>
+              </Stack>
+            ),
+          }}
+        />
+      </Box>
+
+      <Box sx={style.todolist}>
+        <TodosList task={AddTodoText}/>
+      </Box>
+    </>
+  );
 }
---
+++ src/components/TodosList.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function TodosList(props) {
+  return (
+    <>
+      <p>
+        {props.task}
+      </p>
+    </>
+  );
+}
