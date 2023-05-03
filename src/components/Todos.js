b3c43b1305928807298d735c07d1a322ac943797
Archit Ghevariya <ghevariyaarchit@gmail.com>
2023-05-03 23:15:06 +0530

    state issue fixed

--- src/components/Todos.js
+++ src/components/Todos.js
@@ -1,12 +1,12 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Box, TextField, Stack, Button } from "@mui/material";
 import { v4 as uuid } from "uuid";
 //import TodosList from "./TodosList";
 
 export default function Todos() {
+  //Css
   const style = {
     addBox: {
-      //   backgroundColor: "#2d3036",
       borderRadius: 0.3,
       padding: 1,
     },
@@ -30,35 +30,41 @@ export default function Todos() {
     },
   };
 
-  //   Add todo text
-  const [TodoText, SetTodoText] = useState("");
-  const [AddTodoText, SetAddTodoText] = useState([
-    {
-      id: 101,
-      task: "Hello",
-    },
-    {
-      id: 102,
-      task: "Archit",
-    },
-  ]);
-
-  const AddTodo = (e) => {
-    SetAddTodoText([
-      ...AddTodoText,
-      {
-        id: uuid(),
-        task: TodoText,
-      },
-    ]);
-
-    
-    localStorage.setItem("Tasks", JSON.stringify(AddTodoText));
-    let tasksList=localStorage.getItem(AddTodoText.task);
-    console.log(tasksList);
-    SetTodoText("");
-    e.preventDefault();
+  //getting data from local storage and adding to state
+  //if we don't get data then data will be lost after refreshing the page
+  const getTodos = () => {
+    const LocalTodo = localStorage.getItem("Tasks");
+    if (LocalTodo === []) {
+      return [];
+    } else {
+      return JSON.parse(LocalTodo);
+    }
   };
+
+  //Add todo text
+  const [TodoText, SetTodoText] = useState("");
+  const [AddTodoText, SetAddTodoText] = useState(getTodos());
+
+  // Add button handling
+  const AddTodo = (e) => {
+    if (TodoText !== "") {
+      SetAddTodoText([
+        ...AddTodoText,
+        {
+          id: uuid(),
+          task: TodoText,
+        },
+      ]);
+
+      SetTodoText("");
+      e.preventDefault();
+    }
+  };
+
+  // setting data to local storage
+  useEffect(() => {
+    localStorage.setItem("Tasks", JSON.stringify(AddTodoText));
+  }, [AddTodoText]);
 
   return (
     <>
@@ -85,12 +91,12 @@ export default function Todos() {
       </Box>
 
       <Box sx={style.todolist}>
-        {/* <TodosList task={AddTodoText} /> */}
-        {/* {AddTodoText.map((data, key) => (
-          <p key={key}> {localStorage.getItem(data.task)}</p>
-        ))} */}
         {
-         
+        AddTodoText!=null?
+        AddTodoText.map((data, key) =>( 
+          <p key={key}>{data.task}</p>
+        )):""
+          
         }
       </Box>
     </>
