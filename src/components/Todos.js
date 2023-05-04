74e464e6794e5d7bd62ebb34d09aa2387de709b4
Archit Ghevariya <ghevariyaarchit@gmail.com>
2023-05-04 14:45:58 +0530

    add todos done

--- src/components/Todos.js
+++ src/components/Todos.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
-import { Box, TextField, Stack, Button } from "@mui/material";
+import { Box, TextField, Stack, Button, } from "@mui/material";
 import { v4 as uuid } from "uuid";
-//import TodosList from "./TodosList";
+import TodosList from "./TodosList";
 
 export default function Todos() {
   //Css
@@ -34,7 +34,7 @@ export default function Todos() {
   //if we don't get data then data will be lost after refreshing the page
   const getTodos = () => {
     const LocalTodo = localStorage.getItem("Tasks");
-    if (LocalTodo === []) {
+    if (LocalTodo === null) {
       return [];
     } else {
       return JSON.parse(LocalTodo);
@@ -64,6 +64,7 @@ export default function Todos() {
   // setting data to local storage
   useEffect(() => {
     localStorage.setItem("Tasks", JSON.stringify(AddTodoText));
+    
   }, [AddTodoText]);
 
   return (
@@ -91,14 +92,12 @@ export default function Todos() {
       </Box>
 
       <Box sx={style.todolist}>
-        {
-        AddTodoText!=null?
-        AddTodoText.map((data, key) =>( 
-          <p key={key}>{data.task}</p>
-        )):""
-          
-        }
+        
+        <TodosList Textdata={AddTodoText}></TodosList>
+        
       </Box>
+
+      
     </>
   );
 }
--- src/components/TodosList.js
+++ src/components/TodosList.js
@@ -1,11 +1,33 @@
 import React from "react";
+import { Container, Checkbox, FormControlLabel } from "@mui/material";
 
-export default function TodosList() {
+export default function TodosList(props) {
+  const style = {
+    todoContainer: {
+      // backgroundColor: "#22262b",
+      mt: 1,
+      mb: 1,
+      padding: 0.5,
+      borderRadius: 1,
+    },
+    CheckBox: {
+      color:"rgb(232, 232, 232)",
+      borderRadius: 20,
+      
+    },
+  };
   return (
     <>
-      <p>
-        {localStorage.getItem('Task')}
-      </p>
+      {props.Textdata != null
+        ? props.Textdata.map((data, key) => (
+            <Container maxWidth="lg" key={key} sx={style.todoContainer}>
+              <FormControlLabel
+                label={data.task}
+                control={<Checkbox sx={style.CheckBox} color="primary" />}
+              />
+            </Container>
+          ))
+        : ""}
     </>
   );
 }
