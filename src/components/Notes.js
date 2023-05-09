df07c2e35d71b4af583b02c50f39979f19316893
Archit Ghevariya <ghevariyaarchit@gmail.com>
2023-05-10 00:06:40 +0530

    add task container completed

--- src/components/Notes.js
+++ src/components/Notes.js
@@ -1,7 +1,8 @@
 import React from "react";
-import { Box, Divider, Typography, Grid } from "@mui/material";
+import { Box, Divider, Typography, Grid, TextField } from "@mui/material";
 import { FiEdit3 } from "react-icons/fi";
 
+//Style
 const style = {
   MainContainer: {
     display: "flex",
@@ -11,27 +12,29 @@ const style = {
   notesContainer: {
     backgroundColor: "#2d3036",
     width: 250,
-    height: 300,
+    height: 310,
     p: 1.5,
     borderRadius: 1,
     overflow: "hidden",
   },
   contentBox: {
-    height: "230px",
+    height: "231px",
     overflow: "hidden",
-  },
-  contentText: {
-    color: "#BCBCBC",
-    whiteSpace: "normal",
   },
   footer: {
     marginTop: "10px",
   },
   editIcon: {
-    color: "primary",
-    "&:hover": {
-      cursor: "pointer",
+    cursor: "pointer",
+  },
+  TitleInput: {
+    input: {
+      color: "rgb(232, 232, 232)",
     },
+    "& fieldset": { border: "none" },
+  },
+  ContetntInput: {
+    "& fieldset": { border: "none" },
   },
 };
 
@@ -47,21 +50,28 @@ export default function Notes() {
     <>
       <Box sx={style.MainContainer}>
         <Box component="div" sx={style.notesContainer}>
-          <Typography variant="h5">Title</Typography>
+          {/* Title */}
+          <TextField
+            size="small"
+            placeholder="Add Title"
+            fullwidth
+            sx={style.TitleInput}
+          />
+
+          {/* note content */}
           <Box component="div" sx={style.contentBox}>
-            <p style={style.contentText}>
-              Lorem Ipsum is simply dummy text of the printing and typesetting
-              industry. Lorem Ipsum has been the industry's standard dummy text
-              ever since the 1500s, when an unknown printer took a galley of
-              type and scrambled it to make a type specimen book. It has
-              survived not only five centuries, but also the leap into
-              electronic typesetting, remaining essentially unchanged. It was
-              popularised in the 1960s with the release of Letraset sheets
-              containing Lorem Ipsum passages, and more recently with desktop
-              publishing software like Aldus PageMaker including versions of
-              Lorem Ipsum.
-            </p>
+            <TextField
+            id="outlined-multiline-static"
+            fullWidth
+              multiline
+              rows={10}
+              placeholder="Add Note here..."
+              sx={style.ContetntInput}
+              InputProps={{style:{color: "#BCBCBC"}}}
+            />
           </Box>
+
+          {/* footer */}
           <Box component="div" sx={style.footer}>
             <Divider />
             <Grid
@@ -69,10 +79,12 @@ export default function Notes() {
               direction="row"
               justifyContent="space-between"
               alignItems="center"
-              style={{marginTop: 7}}
+              style={{ marginTop: 7 }}
             >
               <Typography variant="p">{today}</Typography>
-              <FiEdit3 sx={style.editIcon} />
+              <Box component="div">
+                <FiEdit3 style={style.editIcon} />
+              </Box>
             </Grid>
           </Box>
         </Box>
